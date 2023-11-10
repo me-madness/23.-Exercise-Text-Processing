899b81a4d6e01246e63532c0497a22a0af3ef5cb
me_madness <p_rangel@abv.bg>
2023-11-10 18:12:07 +0100

    03. done

--- Exercise/03. extract_file.py
+++ Exercise/03. extract_file.py
@@ -1,8 +1,10 @@
 # First task by the lecture
 
-filename_and_extension = input().split("\\")
-
-
+file_path = input().split("\\")
+filename_and_extension = file_path[-1]
+filename, extension = filename_and_extension.input().split(".")
+print(f"File name: {filename}")
+print(f"File extension: {extension}")
 
 # Second task by me
 
