f658ad37f78e059b7759ac54c6f0026628fbb172
me_madness <p_rangel@abv.bg>
2023-11-10 17:53:54 +0100

    02 submit the half task

--- Exercise/02. character_multiplier.py
+++ Exercise/02. character_multiplier.py
@@ -3,8 +3,12 @@
 first_string, second_string = input().split()
 total_sum = 0
 if len(first_string) > len(second_string):
+    # Multiplication
     for index in range(len(second_string)):
+        total_sum += ord(first_string[index]) * ord(second_string[index])
+    for index in range(len(second_string), len(first_string)):
         
+            
 elif len(first_string) < len(second_string):
     pass
 elif len(first_string) == len(second_string):
